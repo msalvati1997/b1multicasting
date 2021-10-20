a05e94539270344365c64b0f2047c574c31d97f5
msalvati97 <salvati.martina@outlook.it>
2021-10-20 19:10:56 +0200

    Upload imports

--- cmd/multicast/routers/router.go
+++ cmd/multicast/routers/router.go
@@ -3,9 +3,11 @@ package routers
 import (
 	beego "github.com/beego/beego/v2/server/web"
 	"github.com/msalvati1997/b1multicasting/cmd/multicast/controllers"
+	"log"
 )
 
 func init() {
+	log.Println("Init root")
 	beego.Router("/", &controllers.RegistryController{})
 	ns := beego.NewNamespace("/v1",
 		beego.NSNamespace("/registry",
