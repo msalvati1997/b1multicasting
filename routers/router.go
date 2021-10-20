14d807fead78dda1bef330e629288d431db4af23
msalvati97 <salvati.martina@outlook.it>
2021-10-20 19:35:28 +0200

    Upload imports

--- routers/router.go
+++ routers/router.go
@@ -2,7 +2,7 @@ package routers
 
 import (
 	beego "github.com/beego/beego/v2/server/web"
-	"github.com/msalvati1997/b1multicasting/cmd/multicast/controllers"
+	"github.com/msalvati1997/b1multicasting/controllers"
 	"log"
 )
 
