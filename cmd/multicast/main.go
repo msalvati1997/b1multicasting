9f430f078ef42c5ca6483374eddc53913b6ec6f4
msalvati97 <salvati.martina@outlook.it>
2021-10-21 19:52:40 +0200

    add swagger

--- cmd/multicast/main.go
+++ cmd/multicast/main.go
@@ -27,21 +27,27 @@ var grpcL net.Listener
 var httpL net.Listener
 var tcpm cmux.CMux
 
+// @title Orders API
+// @version 1.0
+// @description This is a sample service for managing groups multicast
+// @contact.email salvatimartina97@gmail.com
+// @host localhost
+// @BasePath /api
 func main() {
 
-	d := utils.GetEnvIntWithDefault("DELAY", 0)
-	nt := utils.GetEnvIntWithDefault("NUM_THREADS", 1)
-	verbose := utils.GetEnvStringWithDefault("VERBOSE", "ON")
+	//	d := utils.GetEnvIntWithDefault("DELAY", 0)
+	//	nt := utils.GetEnvIntWithDefault("NUM_THREADS", 1)
+	//verbose := utils.GetEnvStringWithDefault("VERBOSE", "ON")
 	rg := utils.GetEnvBoolWithDefault("REGISTRY", false)
 	app := utils.GetEnvBoolWithDefault("APP", false)
 	gPort := utils.GetEnvIntWithDefault("GRPC_PORT", 90)
 	rPort := utils.GetEnvIntWithDefault("REST_PORT", 80)
 
-	delay := flag.Uint("DELAY", uint(d), "delay for sending operations (ms)")
+	//	delay := flag.Uint("DELAY", uint(d), "delay for sending operations (ms)")
 	grpcPort := flag.Uint("GRPC_PORT", uint(gPort), "port number of the grpc server")
 	restPort := flag.Uint("REST_PORT", uint(rPort), "port number of the rest server")
-	numThreads := flag.Uint("NUM_THREADS", uint(nt), "number of threads used to multicast messages")
-	verb := flag.String("VERBOSE", verbose, "Turn verbose mode on or off.")
+	//	numThreads := flag.Uint("NUM_THREADS", uint(nt), "number of threads used to multicast messages")
+	//	verb := flag.String("VERBOSE", verbose, "Turn verbose mode on or off.")
 	registry_addr := flag.String("REGISTRY_ADDR", ":90", "service registry adress")
 	reg := flag.Bool("REGISTRY", rg, "start multicast registry")
 	application := flag.Bool("APP", app, "start multicast application")
@@ -64,6 +70,7 @@ func main() {
 
 	defer func(l net.Listener) {
 		err := l.Close()
+
 		if err != nil {
 
 		}
@@ -78,7 +85,7 @@ func main() {
 	var wg sync.WaitGroup
 	wg.Add(1)
 	go func(w *sync.WaitGroup) {
-		err := StartServer(fmt.Sprintf(":%d", *grpcPort), services...)
+		err := StartServer(services...)
 		if err != nil {
 			log.Println("Error in connecting server", err.Error())
 			return
@@ -88,13 +95,30 @@ func main() {
 
 	wg.Add(1)
 	if *application {
+		api.GrpcPort = *grpcPort
+		newRouter := mux.NewRouter()
+		newRouter.HandleFunc("/groups", api.GetGroups).Methods("GET")
+		newRouter.HandleFunc("/groups", api.CreateGroup).Methods("PUT")
+		newRouter.PathPrefix("/docs/").Handler(httpSwagger.WrapHandler)
 		go func() {
-			err := Run(*grpcPort, *restPort, *registry_addr, *numThreads, *delay, *verb)
+			err := http.Serve(httpL, newRouter)
+			log.Println("http server started.")
 			if err != nil {
-				return
+
 			}
-			wg.Done()
 		}()
+		// Start cmux serving.
+		if err = tcpm.Serve(); !strings.Contains(err.Error(),
+			"use of closed network connection") {
+			log.Fatal(err)
+		}
+
+		log.Println("Server listening on ", restPort)
+
+		api.Registryclient, err = clientregistry.Connect(*registry_addr)
+		if err != nil {
+			log.Println("error", err)
+		}
 	}
 	wgChan := make(chan bool)
 
@@ -108,44 +132,7 @@ func main() {
 	}
 }
 
-// @title Orders API
-// @version 1.0
-// @description This is a sample service for managing groups multicast
-// @contact.email salvatimartina97@gmail.com
-// @host localhost
-// @BasePath /api
-func Run(grpcP uint, restPort uint, registryAddr string, numThreads uint, dl uint, verbose string) error {
-	var err error
-	api.GrpcPort = grpcP
-	newRouter := mux.NewRouter()
-	newRouter.HandleFunc("/groups", api.GetGroups).Methods("GET")
-	newRouter.HandleFunc("/groups", api.CreateGroup).Methods("PUT")
-	newRouter.PathPrefix("/docs/").Handler(httpSwagger.WrapHandler)
-
-	go func() {
-		err := http.Serve(httpL, newRouter)
-		log.Println("http server started.")
-		if err != nil {
-
-		}
-	}()
-
-	//log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", restPort), newRouter))
-	// Start cmux serving.
-	if err = tcpm.Serve(); !strings.Contains(err.Error(),
-		"use of closed network connection") {
-		log.Fatal(err)
-	}
-	log.Println("Server listening on ", restPort)
-	api.Registryclient, err = clientregistry.Connect(registryAddr)
-	if err != nil {
-		log.Println("error", err)
-		return err
-	}
-	return err
-}
-
-func StartServer(programAddress string, grpcServices ...func(grpc.ServiceRegistrar) error) error {
+func StartServer(grpcServices ...func(grpc.ServiceRegistrar) error) error {
 
 	s := grpc.NewServer()
 	for _, grpcService := range grpcServices {
